84b3ac1054bdf6703d21b26872701f2b3846a8c1
yutaka <tsumori-yutaka@codeer.co.jp>
2025-01-16 22:52:02 +0900

    Add color chooser

---
+++ Codeer.LowCode.Bindings.ApexCharts.Designer/ViewModels/DelegateCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+namespace Codeer.LowCode.Bindings.ApexCharts.Designer.ViewModels
+{
+    internal class DelegateCommand(Action action) : ICommand
+    {
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter)
+        {
+            action();
+        }
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
--- Codeer.LowCode.Bindings.ApexCharts.Designer/ViewModels/SeriesViewModel.cs
+++ Codeer.LowCode.Bindings.ApexCharts.Designer/ViewModels/SeriesViewModel.cs
@@ -1,13 +1,24 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using ApexCharts;
+using System.Windows.Input;
+using System.Windows.Media;
+using Codeer.LowCode.Bindings.ApexCharts.Designer.Interop;
 using Codeer.LowCode.Bindings.ApexCharts.Models;
 
 namespace Codeer.LowCode.Bindings.ApexCharts.Designer.ViewModels
 {
-    public class SeriesViewModel(ChartSeriesViewModel viewModel, Series model) : INotifyPropertyChanged
+    public class SeriesViewModel : INotifyPropertyChanged
     {
-        public Series Model { get; } = model;
+        private readonly ChartSeriesViewModel _viewModel;
+
+        public SeriesViewModel(ChartSeriesViewModel viewModel, Series model)
+        {
+            _viewModel = viewModel;
+            Model = model;
+            ChooseColorCommand = new DelegateCommand(OpenColorPicker);
+        }
+
+        public Series Model { get; }
 
         public string Name
         {
@@ -31,7 +42,7 @@ namespace Codeer.LowCode.Bindings.ApexCharts.Designer.ViewModels
             }
         }
 
-        public SeriesType Type
+        public global::ApexCharts.SeriesType Type
         {
             get => Model.Type;
             set
@@ -42,8 +53,10 @@ namespace Codeer.LowCode.Bindings.ApexCharts.Designer.ViewModels
             }
         }
 
-        public IEnumerable<string> NameCandidates => viewModel.NameCandidates;
-        public IEnumerable<string> TypeCandidates => viewModel.TypeCandidates;
+        public ICommand ChooseColorCommand { get; set; }
+
+        public IEnumerable<string> NameCandidates => _viewModel.NameCandidates;
+        public IEnumerable<string> TypeCandidates => _viewModel.TypeCandidates;
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -58,6 +71,37 @@ namespace Codeer.LowCode.Bindings.ApexCharts.Designer.ViewModels
             field = value;
             OnPropertyChanged(propertyName);
             return true;
+        }
+
+        private void OpenColorPicker()
+        {
+            var dialog = new ColorDialog
+            {
+                Color = HexStringToColor(Color)
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                Color = ColorToHexString(dialog.Color);
+            }
+        }
+
+        private string ColorToHexString(Color color) => $"#{color.R:X2}{color.G:X2}{color.B:X2}";
+
+        private Color HexStringToColor(string? hex)
+        {
+            if (string.IsNullOrEmpty(hex))
+            {
+                return Colors.Black;
+            }
+            if (hex.Length != 7 || hex[0] != '#')
+            {
+                return Colors.Black;
+            }
+
+            var r = byte.Parse(hex.Substring(1, 2), System.Globalization.NumberStyles.HexNumber);
+            var g = byte.Parse(hex.Substring(3, 2), System.Globalization.NumberStyles.HexNumber);
+            var b = byte.Parse(hex.Substring(5, 2), System.Globalization.NumberStyles.HexNumber);
+            return System.Windows.Media.Color.FromRgb(r, g, b);
         }
     }
 }
