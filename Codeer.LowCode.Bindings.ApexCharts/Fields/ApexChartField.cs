2ed0a5206b0846d6282da526310c0688188cbfa7
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2025-05-05 16:15:47 +0900

    Hide ToolTip and ToolBar. Color settings are now visible.

--- Codeer.LowCode.Bindings.ApexCharts/Fields/ApexChartField.cs
+++ Codeer.LowCode.Bindings.ApexCharts/Fields/ApexChartField.cs
@@ -18,7 +18,7 @@ namespace Codeer.LowCode.Bindings.ApexCharts.Fields
         private static readonly string[] DefaultTheme = ["#008FFB", "#00E396", "#FEB019", "#FF4560", "#775DD0"];
 
         private List<SeriesData> _data = [];
-        private readonly List<Series> _series;
+        private readonly List<Series> _series = new ();
         private readonly Dictionary<string, ChartAnnotation> _annotations = new();
         private SearchCondition? _additionalCondition;
         private Guid _refreshKey = Guid.NewGuid();
@@ -34,7 +34,7 @@ namespace Codeer.LowCode.Bindings.ApexCharts.Fields
         [ScriptHide]
         public string ModuleName => Design?.SearchCondition.ModuleName ?? string.Empty;
 
-        public ApexChartOptions<SeriesData> Options { get; }
+        public ApexChartOptions<SeriesData> Options { get; } = new ApexChartOptions<SeriesData>();
 
         public List<Series> Series => (Services.AppInfoService.IsDesignMode ? GetDesignSeries() : _series).ToList();
 
@@ -50,15 +50,26 @@ namespace Codeer.LowCode.Bindings.ApexCharts.Fields
         [ScriptHide]
         public string RefreshKey => Design.SeriesType.ToString() + _refreshKey;
 
-        public ApexChartField(ApexChartFieldDesignBase design) : base(design)
+        public ApexChartField(ApexChartFieldDesignBase design) : base(design) { }
+
+        [ScriptHide]
+        public override async Task InitializeDataAsync(FieldDataBase? fieldDataBase)
         {
-            var series = NormalizeSeries(design);
+            if (!this.IsInLayout()) return;
+            InitilaizeCore();
+            await ReloadAsync();
+        }
+
+        void InitilaizeCore()
+        { 
+            var series = NormalizeSeries(Design);
             var isHeatmap = series.FirstOrDefault()?.Type == SeriesType.Heatmap;
-            _series = series.Where(s => (s.Type == SeriesType.Heatmap) == isHeatmap).ToList();
+
+            _series.Clear();
+            _series.AddRange(series.Where(s => (s.Type == SeriesType.Heatmap) == isHeatmap).ToList());
 
             var colors = _series.Select((t, i) => string.IsNullOrEmpty(t.Color) ? DefaultTheme[i % 5] : t.Color).ToList();
 
-            Options = new ApexChartOptions<SeriesData>();
             Options.Chart.Id = "a" + Guid.NewGuid().ToString().Replace("-", "");
             if (Design is ApexChartFieldDesign)
             {
@@ -80,6 +91,12 @@ namespace Codeer.LowCode.Bindings.ApexCharts.Fields
             ];
             Options.Chart ??= new Chart();
             Options.Chart.Height = "100%";
+
+            var color = Color.Or(this.GetFontAppearance()?.Color) ?? "";
+            if (!string.IsNullOrEmpty(color)) Options.Chart.ForeColor = color;
+
+            var backgroundColor = BackgroundColor.Or(this.GetBackgroundColor()) ?? "";
+            if (!string.IsNullOrEmpty(backgroundColor)) Options.Chart.Background = backgroundColor;
 
             if (Design is ApexChartFieldDesign chartDesign)
             {
@@ -109,6 +126,10 @@ namespace Codeer.LowCode.Bindings.ApexCharts.Fields
             Options.Annotations ??= new Annotations();
             Options.Annotations.Xaxis ??= [];
             Options.Annotations.Yaxis ??= [];
+
+            //default disabled
+            Options.Chart.Toolbar = new Toolbar { Show = false };
+            Options.Tooltip = new Tooltip { Enabled = false };
         }
 
         private static List<Series> NormalizeSeries(ApexChartFieldDesignBase design)
@@ -122,12 +143,6 @@ namespace Codeer.LowCode.Bindings.ApexCharts.Fields
                 _ => []
             };
             return series;
-        }
-
-        [ScriptHide]
-        public override async Task InitializeDataAsync(FieldDataBase? fieldDataBase)
-        {
-            if (this.IsInLayout()) await ReloadAsync();
         }
 
         [ScriptName("SetAdditionalCondition")]
@@ -298,5 +313,101 @@ namespace Codeer.LowCode.Bindings.ApexCharts.Fields
 
             NotifyStateChanged();
         }
+    }
+
+    static class DesignHelper
+    {
+        internal static string? Or(this string? that, string? then) => string.IsNullOrEmpty(that) ? then : that;
+
+        internal static IFontAppearance? GetFontAppearance(this FieldBase field)
+        {
+            switch (field.ModuleLayoutType)
+            {
+                case ModuleLayoutType.List:
+                    {
+                        if (!field.Module.Design.ListLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Elements.SelectMany(e => e).FirstOrDefault(e => e.FieldName == field.Design.Name);
+                    }
+                case ModuleLayoutType.Detail:
+                    {
+                        if (!field.Module.Design.DetailLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Layout.GetDescendantFields().FirstOrDefault(e => e.FieldName == field.Design.Name);
+                    }
+                case ModuleLayoutType.Search:
+                    {
+                        if (!field.Module.Design.SearchLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Layout.GetDescendantFields().FirstOrDefault(e => e.FieldName == field.Design.Name);
+                    }
+                default:
+                    return null;
+            }
+        }
+
+        internal static string? GetBackgroundColor(this FieldBase field)
+        {
+            switch (field.ModuleLayoutType)
+            {
+                case ModuleLayoutType.List:
+                    {
+                        if (!field.Module.Design.ListLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Elements.SelectMany(e => e).FirstOrDefault(e => e.FieldName == field.Design.Name)?.BackgroundColor;
+                    }
+                case ModuleLayoutType.Detail:
+                    {
+                        if (!field.Module.Design.DetailLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Layout.GetDescendantFields().FirstOrDefault(e => e.FieldName == field.Design.Name)?.BackgroundColor;
+                    }
+                case ModuleLayoutType.Search:
+                    {
+                        if (!field.Module.Design.SearchLayouts.TryGetValue(field.LayoutName, out var layout)) return null;
+                        return layout.Layout.GetDescendantFields().FirstOrDefault(e => e.FieldName == field.Design.Name)?.BackgroundColor;
+                    }
+                default:
+                    return null;
+            }
+        }
+
+        internal static IEnumerable<FieldLayoutDesign> GetDescendantFields(this LayoutDesignBase layout) =>
+            layout.GetDescendantLayouts().OfType<FieldLayoutDesign>();
+
+        internal static IEnumerable<CanvasLayoutDesign> GetDescendantCanvas(this LayoutDesignBase layout) =>
+            layout.GetDescendantLayouts().OfType<CanvasLayoutDesign>();
+
+
+        internal static List<LayoutDesignBase> GetDescendantLayouts(this LayoutDesignBase? layout)
+        {
+            var list = new List<LayoutDesignBase>();
+            if (layout != null) list.Add(layout);
+
+            if (layout is GridLayoutDesign grid)
+            {
+                foreach (var row in grid.Rows)
+                {
+                    foreach (var col in row.Columns)
+                    {
+                        list.AddRange(col.Layout.GetDescendantLayouts());
+                    }
+                }
+            }
+
+            if (layout is TabLayoutDesign tabLayout)
+            {
+                foreach (var tab in tabLayout.Layouts)
+                {
+                    list.AddRange(tab.GetDescendantLayouts());
+                }
+            }
+
+            if (layout is CanvasLayoutDesign canvas)
+            {
+                foreach (var col in canvas.Elements)
+                {
+                    list.AddRange(col.Layout.GetDescendantLayouts());
+                }
+            }
+
+            return list;
+        }
+
     }
 }
